78e0585773d3d1e3e2ef7c8d9d9fb1173f1ec131
그림자_군주_조연우 <ckhlight2@gmail.com>
2026-01-19 18:01:49 +0900

    Add 디테일
    디테일 추가했습니다.

--- Source/Enemy/Dragon/Public/DragonAI.h
+++ Source/Enemy/Dragon/Public/DragonAI.h
@@ -28,6 +28,11 @@ public:
 
 	virtual void Tick(float DeltaTime) override;
 
+	// 가장 가까운 플레이어를 찾아 타겟으로 지정하고, 시선을 고정(SetFocus)하는 함수
+	// BlueprintCallable을 붙여서 비헤이비어 트리 태스크(BTTask)에서 바로 부를 수 있게 합니다.
+	UFUNCTION(BlueprintCallable, Category = "Dragon AI")
+	AActor* UpdateTargetToNearestPlayer();
+
 protected:
 	/** 부모의 로직(타겟 찾기) + 드래곤 전용 로직(공격 판단) 실행 */
 	virtual void UpdateAIState() override;
@@ -47,5 +52,6 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "Dragon AI")
 	FName BBKey_Phase = "Phase"; // 현재 페이즈 (1, 2, 3...)
 
+	static const FName BBKey_TargetActor;
 	static const FName BBKey_DistanceToTarget;
 };
